a364ecb5073b4dc87ad684a59d5a9d1d4e222259
huubang-hanu <huubang.hanu@gmail.com>
2021-05-16 18:43:23 +0700

    Override init() method in AdminSecurity

--- src/com/ecommerce/filter/AdminSecurity.java
+++ src/com/ecommerce/filter/AdminSecurity.java
@@ -3,6 +3,7 @@ package com.ecommerce.filter;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -18,6 +19,11 @@ import com.ecommerce.model.User;
  */
 @WebFilter("/admin/*")
 public class AdminSecurity implements Filter {
+	@Override
+		public void init(FilterConfig filterConfig) throws ServletException {
+			// TODO Auto-generated method stub
+			Filter.super.init(filterConfig);
+		}
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
